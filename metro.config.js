6ebcaeb71df295c943c3da6e775d553c23156ee3
Satrio <56964640+satriowaskitho@users.noreply.github.com>
2026-05-12 11:24:05 +0700

    Add .wasm to asset extensions in metro config
    Allow .wasm files to be treated as static assets for expo-sqlite web build.

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Allow .wasm files to be treated as static assets (fixes expo-sqlite web build)
+config.resolver.assetExts.push('wasm');
+
 module.exports = withNativeWind(config, { input: './global.css' });
